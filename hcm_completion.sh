0e99bb223e08cc5920c19c2c3c0f94274756c002
Nicuvëo <crucuny@gmail.com>
2015-09-03 18:01:35 +0200

    Added bash completion.

---
+++ hcm_completion.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash
+
+_hcm()
+{
+    local cur_word
+    local cards
+    local cmds
+
+    cmd="${COMP_WORDS[1]}"
+    arg="${COMP_WORDS[COMP_CWORD]#\"}"
+    cmds="help add del stats update input list"
+    filters="\
+ owned\
+ q=0\
+ q=1\
+ q=2\
+ h=Druid\
+ h=Hunter\
+ h=Mage\
+ h=Paladin\
+ h=Priest\
+ h=Rogue\
+ h=Shaman\
+ h=Warlock\
+ h=Warrior\
+ h=Neutral\
+ r=Common\
+ r=Rare\
+ r=Epic\
+ r=Legendary\
+ s=c\
+ s=gvg\
+ s=tgt\
+ c="
+
+    if [ "$COMP_CWORD" = 1 ] ; then
+        COMPREPLY=( $(compgen -W "$cmds" -- ${arg}) )
+    elif [ "$COMP_CWORD" -ge 2 ] && [ "$cmd" = "list" -o "$cmd" = "input" ] ; then
+        COMPREPLY=( $(compgen -W "$filters" -- ${arg}) )
+    elif [ "$COMP_CWORD" -ge 2 ] && [ "$cmd" = "add" -o "$cmd" = "del" ] ; then
+        local cards=$(hcm names | egrep -i "^$arg" | sed 's/.*/"\\"&\\""/')
+        eval "COMPREPLY=( $cards )"
+    else
+        COMPREPLY=()
+    fi
+    return 0
+}
+
+complete -F _hcm hcm
