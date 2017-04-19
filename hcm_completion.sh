f5a0d223646f629c493b45377284af7327bc8f7d
Nicuvëo <crucuny@gmail.com>
2017-04-19 10:38:05 +0100

    Updated bash completion.

--- hcm_completion.sh
+++ hcm_completion.sh
@@ -8,10 +8,11 @@ _hcm()
 
     cmd="${COMP_WORDS[1]}"
     arg="${COMP_WORDS[COMP_CWORD]#\"}"
-    cmds="help add del stats update input list"
+    cmds="help list stats update add del input fix"
     filters="\
  owned\
  missing\
+ standard\
  q=0\
  q=1\
  q=2\
@@ -29,17 +30,21 @@ _hcm()
  r=Rare\
  r=Epic\
  r=Legendary\
- s=c\
+ s=cla\
+ s=hof\
  s=gvg\
  s=tgt\
  s=wog\
- s=gad\
+ s=msg\
+ s=jtu\
  c="
 
     if [ "$COMP_CWORD" = 1 ] ; then
         COMPREPLY=( $(compgen -W "$cmds" -- ${arg}) )
     elif [ "$COMP_CWORD" -ge 2 ] && [ "$cmd" = "list" -o "$cmd" = "input" ] ; then
         COMPREPLY=( $(compgen -W "$filters" -- ${arg}) )
+    elif [ "$COMP_CWORD" -ge 2 ] && [ "$cmd" = "stats" ] ; then
+        COMPREPLY=( $(compgen -W "wild standard" -- ${arg}) )
     elif [ "$COMP_CWORD" -ge 2 ] && [ "$cmd" = "add" -o "$cmd" = "del" ] ; then
         local cards=$(hcm names | egrep -i "^$arg" | sed 's/.*/"\\"&\\""/')
         eval "COMPREPLY=( $cards )"
